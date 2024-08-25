a8e5f44417bdb28f29103e070c763880fc0f8caf
emirkara42 <emirkaraxyz@gmail.com>
2024-08-25 11:46:38 +0300

    added highest score

--- main.py
+++ main.py
@@ -35,12 +35,12 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            data = int(file.read())
+            self.high_score = data
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -13,13 +16,22 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
+        #self.clear()
         self.score += 1
         self.update_scoreboard()
